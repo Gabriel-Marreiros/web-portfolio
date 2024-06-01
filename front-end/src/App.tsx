8046ed91d209a0b8b6da65e4c9d64dd576cbf97c
Gabriel Marreiros <gabriel.s.marreiros@hotmail.com>
2024-06-01 01:08:13 -0300

    feat(frontend): configuração geral da estrutura do projeto

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,29 @@
+import Home from "./components/Home"
+import Projects from "./components/Projects"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import AboutMe from "./components/AboutMe"
+import Skills from "./components/Skills"
+import Certifications from "./components/Certifications"
+import Contacts from "./components/Contacts"
 
 
 function App() {
 
   return (
     <>
+      <Navbar />
+
+      <main data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" className="scrollspy-example" tabIndex={1}>
+        <Home />
+        <AboutMe/>
+        <Skills />
+        <Projects />
+        <Certifications />
+        <Contacts />
+      </main>
+      
+      <Footer />
     </>
   )
 }
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import 'rsuite/dist/rsuite.min.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
