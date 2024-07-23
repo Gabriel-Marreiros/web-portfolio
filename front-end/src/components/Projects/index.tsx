21e55f80c53ab9710b9849e0804a3c0b0c3d4ad1
Gabriel Marreiros <gabriel.s.marreiros@hotmail.com>
2024-07-23 14:48:32 -0300

    feat(frontend): adiciona um texto descritivo na seção de projetos

--- front-end/src/components/Projects/index.tsx
+++ front-end/src/components/Projects/index.tsx
@@ -12,6 +12,10 @@ function Projects(){
 
             <SectionTitle title="Projetos"/>
 
+            <p className={`mt-1 p-2 p-lg-4 p-xl-5 fs-4 text-center text-white`}>
+                Todos os projetos, incluindo bancos de dados <span className={`text-danger`}>MySQL</span> e <span className={`text-danger`}>MongoDB</span>, estão hospedados em um servidor <span className={`text-danger`}>VPS</span> particular, rodando em contêineres <span className={`text-danger`}>Docker</span>. O ambiente foi configurado utilizando um servidor <span className={`text-danger`}>Nginx</span> com proxy reverso e certificado SSL digital. Todos os projetos possuem automações de <span className={`text-danger`}>CI/CD</span> utilizando o <span className={`text-danger`}>GitHub Actions</span> para executar os testes unitários e fazer o deploy dos projetos no servidor.
+            </p>
+
             <div className={`pt-5 px-lg-5 ${styles.projects_container}`}>
                 { projects.map((project: IProject, i) => ( 
                     <ProjectCard key={i} project={project} id={i}/> 
